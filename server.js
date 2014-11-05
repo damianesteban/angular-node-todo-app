6f7070346a5dfe0c5d88e681b133017ab392ef24
Damian Esteban <damian.esteban@gmail.com>
2014-11-05 14:23:35 -0500

    add port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // server.js
 
-    // set up
-    var express        = require('express');
-    var app            = express();                  // create our app w/ express
-    var mongoose       = require('mongoose');        // mongoose for mongodb
-    var morgan         = require('morgan');          // log requests to the console (express4)
-    var bodyParser     = require('body-parser');     // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+// set up
+var express        = require('express');
+var app            = express();                  // create our app w/ express
+var mongoose       = require('mongoose');        // mongoose for mongodb
+var morgan         = require('morgan');          // log requests to the console (express4)
+var bodyParser     = require('body-parser');     // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
     // configuration ========================
 
