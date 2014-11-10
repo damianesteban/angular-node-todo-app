b2734e37aef44df7c0190b665c4f64bff484eb92
Damian Esteban <damian.esteban@gmail.com>
2014-11-10 15:23:34 -0500

    moved database  routes definitions into separate files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+
+    module.exports = {
+        url : 'mongodb://esteban:esteban@ds051630.mongolab.com:51630/angular-node-todo-app'
+    };
+
--- server.js
+++ server.js
@@ -1,92 +1,30 @@
 // server.js
 
-// set up
+// set up ============================================================
 var express        = require('express');
-var app            = express();                  // create our app w/ express
-var mongoose       = require('mongoose');        // mongoose for mongodb
-var morgan         = require('morgan');          // log requests to the console (express4)
-var bodyParser     = require('body-parser');     // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var app            = express();                    // create our app w/ express
+var mongoose       = require('mongoose');          // mongoose for mongodb
+var port           = process.env.PORT || 8080;     // set the port
+var database       = require('./config/database'); // load the database config
+var morgan         = require('morgan');            // log requests to console (express 4)
+var bodyParser     = require('body-parser');       // pull information from HTML POST (express 4)
+var methodOverride = require('method-override');   // simulate DELETE and PUT (express 4)
 
-    // configuration ========================
-    mongoose.connect('mongodb://esteban:esteban@ds051630.mongolab.com:51630/angular-node-todo-app');
-    app.use(express.static(__dirname + '/public'));  // set the static files location, /public/img will
-                                                     // be /img for users.
-    app.use(morgan('dev')); // log every request to the console
-    app.use(bodyParser.urlencoded({ 'extended':'true' }));  //parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());  // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+// configuration =====================================================
+mongoose.connect(database.url);  // connect to mongoDB database
 
-    app.use(methodOverride());
+app.configure(function() {
+    app.use(express.static(__dir.name + '/public')); // set the static files location /public/img will be /img for users.
+    app.use(express.logger('dev')); 						// log every request to the console
+    app.use(express.bodyParser()); 							// pull information from html in POST
+    app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
 
+// routes ======================================================================
+require('./app/routes.js')(app);
 
-        // define model
-        var Todo = mongoose.model('Todo', {
-            text : String
-        });
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
 
-// routes ====================================
-    // api -----------------------------------
-    // get all todos
-    app.get('/api/todos', function(req, res) {
-
-      // use mongoose to get all todos in the database
-      Todo.find(function(err, todos) {
-
-          // if there is an error retrieving, send the error.  Nothing after
-          // res.send(err) will execute
-          if (err)
-              res.send(err)
-
-          res.json(todos);   // return all todos in JSON format.
-      });
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-
-        Todo.create({
-          text: req.body.text,
-          done: false
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-          _id: req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err)
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-    });
-
-    // application-----------------------
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html');  // load the single view file (angular will handle the
-                                              // the page changes on the front-end
-    });
-    // listen (start app with node server.js) ==================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
 
